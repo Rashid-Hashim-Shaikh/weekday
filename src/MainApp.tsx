749e5036727d50ae702135e8bdba7c6037daacfe
Rashid-Hashim-Shaikh <contactrashid8@gmail.com>
2024-05-04 19:55:03 +0530

    :clown_face: Add mock api response
    Added mock api response to not put load on backend
    server while development

--- src/MainApp.tsx
+++ src/MainApp.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
-import { Typography } from '@mui/material';
+import { useEffect } from 'react';
+import { FilterSection, JobList } from './component';
+import mockApiResponse from './data/mockApi.json';
 
-const MainApp = () => (
-  <div>
-    <Typography>
-      Test typography font
-    </Typography>
-  </div>
-);
+const MainApp = () => {
+  console.log(mockApiResponse);
+  useEffect(() => {
+    // const myHeaders = new Headers();
+    // myHeaders.append('Content-Type', 'application/json');
+
+    // const body = JSON.stringify({
+    //   limit: 10,
+    //   offset: 0,
+    // });
+
+    // const requestOptions = {
+    //   method: 'POST',
+    //   headers: myHeaders,
+    //   body,
+    // };
+    // const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT;
+    // fetch(API_ENDPOINT, requestOptions)
+    //   .then((response) => response.text())
+    //   .then((result) => console.log(result))
+    //   .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <div>
+      <FilterSection />
+      <JobList />
+    </div>
+  );
+};
 
 export default MainApp;
