1c90c6a4104bd3dee87931c6dd0868e83b19c355
Rashid-Hashim-Shaikh <contactrashid8@gmail.com>
2024-05-04 16:45:52 +0530

    :wrench: Configure Eslint
    Configured airbnb eslint typescript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,23 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    "plugin:react/jsx-runtime",
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@typescript-eslint'],
+  parserOptions: {
+    project: './tsconfig.json'
+  },
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'linebreak-style': 0,
+    'react/function-component-definition': 0,
   },
-}
+};
