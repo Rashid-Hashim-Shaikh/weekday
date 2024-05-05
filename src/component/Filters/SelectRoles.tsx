99faff060b927f10f90f927f7cfe058a8b441850
Rashid-Hashim-Shaikh <contactrashid8@gmail.com>
2024-05-05 08:01:23 +0530

    :recycle: Refactor group selection logic and fix ts errors

--- src/component/Filters/SelectRoles.tsx
+++ src/component/Filters/SelectRoles.tsx
@@ -1,33 +1,39 @@
 import { useState } from 'react';
-import Select from 'react-select';
+import Select, { ActionMeta, ValueType } from 'react-select';
 import { Box } from '@mui/material';
-import { groupedRolesOption } from '../../constants/options';
+import { groupedRolesOption, GroupedRolesOption, RolesOption } from '../../constants/options';
 
 const LABEL = 'Role';
 
 const SelectRoles = () => {
-  const [selectedValues, setSelectedValues] = useState([]);
+  const [selectedValues, setSelectedValues] = useState<RolesOption[] | []>([]);
 
   // TODO: Handling filtering logic pending and few types declaration pending
-  const handleChange = (value) => {
+  // eslint-disable-next-line max-len
+  const handleChange = (value: ValueType<RolesOption[], true>, actionMeta: ActionMeta<RolesOption>) => {
     // Check if the selected value belongs to the same group (here role)
     // as the previously selected value. If the group/role is different,
     // remove the previously selected value and add the newly selected value.
-    setSelectedValues((prev) => {
-      if (prev?.length === 0
-      || prev.map((v) => v?.role).includes(value[value.length - 1]?.role)) {
-        return value;
-      }
-      return [value[value?.length - 1]];
-    });
+
+    if (value && actionMeta.action === 'select-option') {
+      setSelectedValues((prev) => {
+        if (prev?.length === 0
+      || prev.map((v) => v?.role).includes(actionMeta?.option?.role)) {
+          return value;
+        }
+        return [actionMeta.option];
+      });
+    } else {
+      setSelectedValues(value);
+    }
   };
 
-  const formatGroupLabel = (data) => (
+  const formatGroupLabel = (data: GroupedRolesOption) => (
     <div style={{ display: 'flex', justifyContent: 'space-between' }}>
       <span>{data.label}</span>
     </div>
   );
-// TODO: Style refinement pending
+  // TODO: Style refinement pending
   return (
     <Box sx={{
       minWidth: 200,
